92ef4b94c0e31006b3ca04e1c1d61784cc0e9deb
Jeremy Adams <adamsj@objectcomputing.com>
2021-02-15 14:37:11 -0600

    clarity to text output

--- start_docker.sh
+++ start_docker.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+usage()
+{
+    printf "defaults:\n image = %s\n tag = %s\n both = %s\n container = %s\n path = %s\n dds path = %s\n net host = %s\n" $image $tag $imagetag $container $file_path $dds_volume_map $nethost
+    printf "options:\n [-i] image name\n [-t] change tag\n [-b] both image and tag\n [-c] container name\n [-p] path/to/volume/map\n [-d] dds source path\n [-n] use --net=host option to share host network\n"
+}
+
 file_path=`pwd`
 dds_volume_map=""
 container=`basename $file_path`"_opendds-f"
@@ -8,6 +15,7 @@ imagetag=$image":"$tag
 prev_image=$image
 prev_tag=$tag
 nethost=""
+
 while getopts ":hp:c:i:d:nt:b:" opt; do
 case ${opt} in
     p )
@@ -32,17 +40,16 @@ case ${opt} in
         imagetag=$OPTARG
     ;;
     h )
-        printf "defaults:\n image = %s\n tag = %s\n both = %s\n container = %s\n path = %s\n dds path = %s\n net host = %s\n" $image $tag $imagetag $container $file_path $dds_volume_map $nethost
-        printf "options:\n [-i] image name\n [-t] change tag\n [-b] both image and tag\n [-c] container name\n [-p] path/to/volume/map\n [-d] dds source path\n [-n] use --net=host option to share host network\n"
-    exit
+        usage
+        exit
     ;;
 esac
 done
 
-echo $imagetag
+echo "default image:tag = "$imagetag
 if [ $image != $prev_image ] || [ $tag != $prev_tag ];then
     imagetag=$image":"$tag
-    echo "changing imagetag"
+    echo "changing image:tag to "$imagetag
 fi
 
 docker ps -f "name=$container"|grep -v CONTAINER >/dev/null
