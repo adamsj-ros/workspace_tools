94ad350aa11e796d9dfa117d9fe5dccb9661b722
Jeremy <jeremy.adams@fireflyautomatix.com>
2021-04-29 20:05:31 -0500

    generalize for selected shell

--- add_sh_symlinks.sh
+++ add_sh_symlinks.sh
@@ -2,13 +2,13 @@
 
 script_path=${0:a:h}
 if [ ! -f $HOME/.zsh_aliases ]; then
-    ln -sf $script_path/zsh_aliases $HOME/.zsh_aliases
+    ln -sf $script_path/sh_aliases $HOME/.zsh_aliases
 fi
 if [ ! -f $HOME/.zshrc ]; then
     ln -sf $script_path/zshrc $HOME/.zshrc
 fi
 if [ ! -f $HOME/.bash_aliases ]; then
-    ln -sf $script_path/zsh_aliases $HOME/.bash_aliases
+    ln -sf $script_path/sh_aliases $HOME/.bash_aliases
 fi
 if [ ! -f $HOME/.bashrc ]; then
     ln -sf $script_path/bashrc $HOME/.bashrc
