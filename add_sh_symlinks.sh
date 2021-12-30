06b41c82413c0da1ecad963d061e06c36e835924
Jeremy <jeremy.adams@fireflyautomatix.com>
2021-12-30 15:02:57 -0600

    oh my zsh and p10k install

--- add_sh_symlinks.sh
+++ add_sh_symlinks.sh
@@ -1,5 +1,30 @@
 #!/usr/bin/env zsh
 
+#oh-my-zsh
+curlfound=0
+omz=0
+if [ ! -d $HOME/.oh-my-zsh ];then
+    which curl
+    if [ $? -eq 0 ];then
+        echo "installing oh-my-zsh via curl"
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        curlfound=1
+        omz=1
+    fi
+    which wget
+    if [  $? -eq 0 ] && [ "$curlfound" = 0 ]; then
+        echo "installing oh-my-zsh via wget"
+        sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+        omz=1
+    fi
+fi
+
+#p10k theme
+if [ "$omz" = 1 ] && [ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ];then
+    echo "installing p10k theme for oh-my-zsh"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
+
 script_path=${0:a:h}
 if [ ! -f $HOME/.zsh_aliases ]; then
     ln -sf $script_path/sh_aliases $HOME/.zsh_aliases
