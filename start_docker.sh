f3b4ba9b3868e6fd95dd92c1e8327cb216643502
adamsj-ros <j2@adams-assoc.com>
2022-11-23 17:28:32 -0600

    updates from mac os

--- start_docker.sh
+++ start_docker.sh
@@ -4,13 +4,13 @@
 usage()
 {
     printf "defaults:\n image = %s\n tag = %s\n both = %s\n container = %s\n path = %s\n net host = %s\n shell = %s\n" $image $tag $imagetag $container $file_path $network $shell
-    printf "options:\n [-i] image name\n [-t] change tag\n [-b] both image and tag\n [-c] container name\n [-p] path/to/volume/map\n [-n] use another netowrk option like none, host, a container net, or docker network\n [-z] use zsh (if installed in image) \n"
+    printf "options:\n [-i] image name\n [-t] change tag\n [-b] both image and tag\n [-c] container name\n [-p] path/to/volume/map\n [-n] use another netowrk option like none, host, a container net, or docker network\n [-r] run rocker\n [-z] use zsh (if installed in image) \n"
 }
 
 file_path=`pwd`
 container=`basename $file_path`"-container"
 image="ros"
-tag="foxy"
+tag="humble"
 imagetag=$image":"$tag
 imagetagstring=$image"_"$tag
 prev_image=$image
@@ -18,8 +18,9 @@ prev_tag=$tag
 prev_imagetag=$imagetag
 network="host"
 shell="bash"
+d_or_r="docker"
 
-while getopts ":hp:c:i:n:t:b:z" opt; do
+while getopts ":hp:c:i:n:t:b:zr" opt; do
 case ${opt} in
     p )
         file_path=$OPTARG
@@ -44,6 +45,9 @@ case ${opt} in
     z )
         shell="zsh"
     ;;
+    r )
+        d_or_r="rocker"
+    ;;
     h )
         usage
         exit
@@ -59,7 +63,7 @@ elif [ $imagetag != prev_imagetag ];then
 else
     echo -e "${TXTGRN}using default image:tag = "$imagetag${TXTRST}
 fi
-imagetagstring=`echo $imagetag|sed --expression 's/:/_/g'`
+imagetagstring=`echo $imagetag|sed -e 's/:/_/g'`
 container=$imagetagstring"_"$container
 
 docker ps -f "name=$container"|grep -v CONTAINER >/dev/null
@@ -67,9 +71,10 @@ start_container=$?
 
 if [ $start_container == 1 ]; then
     echo -e "${TXTGRN}starting "$container" at "$file_path" from "$imagetag${TXTRST}
-    if [ `uname` = "Darwin" ];then
-        docker run --net=$network -d --rm --name $container -v $file_path:/opt/workspace $imagetag $shell -c "while true; do sleep 5; done"
-    elif [ `uname` = "Linux" ];then
+    if [ $d_or_r = "docker" ];then
+        #docker run -v host_path:container_path
+        docker run --net=$network -d --rm --name $container -v $file_path:$file_path $imagetag $shell -c "while true; do sleep 5; done"
+    elif [ $d_or_r = "rocker" ];then
         #https://github.com/osrf/rocker#installation
         rocker --home --oyr-run-arg " --privileged" --name $container --network $network --nvidia --ssh --user --x11 $imagetag $shell
     fi
